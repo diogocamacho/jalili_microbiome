0364d42d2192a1975ed21528b0cfa27fb7087cc0
diogocamacho <diogo.camacho.2008@gmail.com>
2018-07-05 22:09:12 -0400

    - added human stock comparison - added richness analysis in human stool data - edits to richness_analysis.R - edits to beta_diversity_test.R

--- R/beta_diversity_test.R
+++ R/beta_diversity_test.R
@@ -39,55 +39,72 @@ permanova_day3 <- adonis(formula = t(y1) ~ condition,
                          permutations = 1000)
 
 
-# PERMANOVA on human samples vs sasan's data
-# tmp1 <- tidy_full$taxonomy$Genus
-# tmp2 <- tidy_human$taxonomy$genus
-# common_otus <- intersect(tmp1,tmp2)
-# 
-# id1 <- which(tidy_full$taxonomy$Genus %in% common_otus)
-# id2 <- which(tidy_human$taxonomy$genus %in% common_otus)
-# comb1 <- as.matrix(tidy_full$otu_table)[id1, ]
-# comb2 <- as.matrix(tidy_human$otu_table)[id2, ]
-# d1 <- as.vector(comb1)
-# d2 <- as.vector(comb2)
-# 
-# otu_mat <- data_frame(genus = c(rep(tidy_full$taxonomy$Genus[id1], ncol(comb1)), 
-#                                 rep(tidy_human$taxonomy$genus[id2], ncol(comb2))),
-#                       counts = c(d1, d2),
-#                       relative_abundance = c(as.vector(apply(comb1, 2, function(x) x / sum(x))),
-#                                              as.vector(apply(comb2, 2, function(x) x / sum(x)))),
-#                       minmax_counts = c(as.vector(apply(comb1, 2, function(x) (x - min(x)) / (max(x) - min(x)))),
-#                                         as.vector(apply(comb2, 2, function(x) (x - min(x)) / (max(x) - min(x))))),
-#                       scaled_data = c(as.vector(scale(comb1)),
-#                                    as.vector(scale(comb2))),
-#                       condition = c(as.vector(sapply(tidy_full$samples$condition, rep, nrow(comb1))),
-#                                     rep("human_stool", length(d2))))
-# 
-# otu_mat %>% 
-#   dplyr::filter(., counts != 0) %>% 
-#   ggplot() + 
-#   # geom_violin(aes(x = genus, y = log10(counts), fill = condition)) +
-#   # geom_boxplot(aes(x = genus, y = log10(counts), fill = condition), outlier.alpha = 0) +
-#   # geom_boxplot(aes(x = genus, y = log10(counts), fill = condition), notch = TRUE) +
-#   # geom_boxplot(aes(x = genus, y = minmax_counts, fill = condition), outlier.alpha = 0) +
-#   # geom_boxplot(aes(x = genus, y = scaled_data, fill = condition)) + 
-#   # geom_point(aes(x = genus, y = log10(counts), color = condition), position = position_jitterdodge(jitter.width = 0.2)) +
-#   geom_point(aes(x = genus, y = relative_abundance, color = condition), position = position_jitterdodge(jitter.width = 0.1), size = 3) +
-#   # ylim(c(0, 0.5)) + 
-#   # facet_grid(. ~ genus, scales = "free") +
-#   # scale_fill_manual(values = c("#006699", "#00CCFF", "#FF3300"), 
-#   #                   breaks = c("aerobic", "anaerobic", "human_stool"), 
-#   #                   labels = c("Aerobic", "Anaerobic", "Human stool"), 
-#   #                   name = "Sample type") + 
-#   scale_color_manual(values = c("#006699", "#00CCFF", "#FF3300"), 
-#                     breaks = c("aerobic", "anaerobic", "human_stool"), 
-#                     labels = c("Aerobic", "Anaerobic", "Human stool"), 
-#                     name = "Sample type") + 
-#   labs(x = "Genus", y = "log10(counts)") + 
-#   theme_bw() + 
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 12, color = "black"),
-#         axis.text.y = element_text(size = 12, color = "black"),
-#         axis.title = element_text(size = 20, face = "bold", color = "black"),
-#         panel.grid = element_blank())
-#   
-# 
+# PERMANOVA on human samples vs anaerobic chip data
+all_phyla <- unique(c(tidy_otu$taxonomy$Genus,
+               tidy_humans$taxonomy$genus))
+
+d1 <- matrix(0, nrow = length(all_phyla), ncol = ncol(tidy_otu$otu_table))
+d2 <- matrix(0, nrow = length(all_phyla), ncol = ncol(tidy_humans$otu_table))
+for (i in seq(1, length(all_phyla))) {
+  y1 <- which(tidy_otu$taxonomy$Genus == all_phyla[i])
+  y2 <- which(tidy_humans$taxonomy$genus == all_phyla[i])
+  
+  if (length(y1) > 1) {
+    c1 <- Matrix::colSums(tidy_otu$otu_table[y1, ]) 
+    d1[i, ] <- c1
+  } else if (length(y1) == 1) {
+    d1[i, ] <- tidy_otu$otu_table[y1, ]
+  }
+  
+  if (length(y2) > 1) {
+    c2 <- Matrix::colSums(tidy_humans$otu_table[y2, ])  
+    d2[i, ] <- c2
+  } else if (length(y2) == 1) {
+    d2[i, ] <- tidy_humans$otu_table[y2, ]
+  }
+}
+
+d3 <- cbind(d1, d2)
+colnames(d3) <- c(tidy_otu$samples$filename,
+                  tidy_humans$samples$X.SampleID)
+
+# permanova against human samples ----
+d <- vegan::vegdist(x = t(counts), method = "bray") # <-- distance between samples!
+
+# random sampling of human data (don't need all the possible samples)
+permanova_human <- vector(mode = "list", length = 1000)
+
+for (i in 1:1000) {
+  sid <- sample(x = ncol(d2), size = ncol(d1), replace = FALSE)
+  
+  metadata <- data.frame(filename = c(tidy_otu$samples$filename,
+                                      paste0("X", tidy_humans$samples$X.SampleID[sid])),
+                         condition = c(tidy_otu$samples$condition,
+                                       rep("human_stool", length(sid))))
+  
+  d3 <- cbind(d1, d2[, sid])
+  colnames(d3) <- c(as.character(tidy_otu$samples$filename),
+                    paste0("X", tidy_humans$samples$X.SampleID[sid]))
+  counts <- d3
+  
+  
+  x1 <- metadata[which(metadata$condition == "aerobic" | metadata$condition == "human_stool"), ]
+  y1 <- counts[, which(colnames(counts) %in% x1$filename)]
+  human_aerobic_permanova <- adonis(formula = t(counts) ~ condition,
+                                    data = metadata,
+                                    method = "bray",
+                                    permutations = 1000)
+  
+  x1 <- metadata[which(metadata$condition == "anaerobic" | metadata$condition == "human_stool"), ]
+  y1 <- counts[, which(colnames(counts) %in% x1$filename)]
+  human_anaerobic_permanova <- adonis(formula = t(counts) ~ condition,
+                                      data = metadata,
+                                      method = "bray",
+                                      permutations = 1000)
+  
+  permanova_human[[i]] <- data_frame(anaerobic = human_anaerobic_permanova$aov.tab$`Pr(>F)`[1],
+                                     aerobic = human_aerobic_permanova$aov.tab$`Pr(>F)`[1])
+  
+}
+permanova_human <- dplyr::bind_rows(permanova_human)
+
--- R/richness_analysis.R
+++ R/richness_analysis.R
@@ -10,4 +10,34 @@ richness_data <- data_frame(metric = as.vector(sapply(colnames(richness_data), r
                             day = as.vector(t(sapply(sample_data(full_OTU)[, 5], rep, ncol(richness_data)))),
                             cond_day = as.vector(t(sapply(sample_data(full_OTU)[, 8], rep, ncol(richness_data)))))
 
+# add human data
+rich_human <- phyloseq::estimate_richness(HMPv35)
+stid <- sample_data(HMPv35)[which(sample_data(HMPv35)[, 6] == "Stool"), 1]
+rich_human <- rich_human[which(gsub("X", "", rownames(rich_human)) %in% stid$X.SampleID), ]
+
+richness_data <- data_frame(metric = as.vector(sapply(colnames(richness_data), rep, nrow(richness_data))),
+                            alpha_diversity = as.vector(as.matrix(richness_data)),
+                            sample_name = as.vector(t(sapply(rownames(richness_data), rep, ncol(richness_data)))),
+                            condition = as.vector(t(sapply(sample_data(full_OTU)[, 6], rep, ncol(richness_data)))),
+                            replicate = as.vector(t(sapply(sample_data(full_OTU)[, 7], rep, ncol(richness_data)))),
+                            day = as.vector(t(sapply(sample_data(full_OTU)[, 5], rep, ncol(richness_data)))),
+                            cond_day = as.vector(t(sapply(sample_data(full_OTU)[, 8], rep, ncol(richness_data)))))
+
+combo_richness <- data_frame(metric = c(richness_data$metric, 
+                                        as.vector(sapply(colnames(rich_human), rep, nrow(rich_human)))),
+                             alpha_diversity = c(richness_data$alpha_diversity,
+                                                 as.vector(as.matrix(rich_human))),
+                             sample_name = c(richness_data$sample_name, 
+                                             rep("human_stool", nrow(rich_human) * ncol(rich_human))),
+                             condition = c(richness_data$condition, 
+                                           rep("human_stool", nrow(rich_human) * ncol(rich_human))),
+                             replicate = c(richness_data$replicate,
+                                           rep(1, nrow(rich_human) * ncol(rich_human))),
+                             day = c(richness_data$day,
+                                     rep(NA, nrow(rich_human) * ncol(rich_human))),
+                             cond_day = c(richness_data$cond_day,
+                                          rep(1, nrow(rich_human) * ncol(rich_human))))
+
+
+
 
