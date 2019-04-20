352500b806a556773faa1714d38c340e3d21aca4
Diogo Camacho <diogocamacho@mordor.fios-router.home>
2019-04-20 10:35:50 -0400

    pushing all edits

--- R/figure_3.R
+++ R/figure_3.R
@@ -1,4 +1,5 @@
 # FIGURE 3 ----
+fig_dev <- "tiff"
 
 # Figure 3a: Diversity analysis 
 strip_labels <- c("1" = "Day 1",
@@ -6,21 +7,28 @@ strip_labels <- c("1" = "Day 1",
                   "3" = "Day 3")
 
 richness_data %>% 
+  dplyr::mutate(., condition = replace(condition, condition == "aerobic", "Aerobic")) %>% 
+  dplyr::mutate(., condition = replace(condition, condition == "anaerobic", "Anaerobic")) %>%
   dplyr::filter(., metric == "Observed", !is.na(condition)) %>% 
   ggplot() + 
   geom_boxplot(aes(x = condition, y = alpha_diversity), outlier.size = 0) +
   geom_point(aes(x = condition, y = alpha_diversity, fill = condition), pch = 21, size = 7, color = "black") +
   scale_fill_manual(values = c("#006699", "#00CCFF"), breaks = c("aerobic", "anaerobic")) + 
-  labs(x=NULL,y="Observed richness") + 
-  facet_grid(~ day, labeller = as_labeller(strip_labels)) +
+  labs(x = NULL, y = "Observed richness") + 
+  facet_wrap(~ day, labeller = as_labeller(strip_labels), scales = "free") +
+  scale_y_continuous(limits = c(100, 300)) +
   theme_bw() + 
   theme(panel.grid = element_blank(),
         strip.background = element_blank(),
         strip.text.x = element_text(size = 24, face = "bold", color = "black"),
-        axis.text = element_text(size = 24, color = "black", face = "bold"),
+        axis.text.y = element_text(size = 24, color = "black", face = "bold"),
+        axis.text.x = element_text(size = 24, color = "black", face = "bold", angle = 45, hjust = 1, vjust = 1),
         axis.title = element_text(size = 32, face = "bold", color = "black"),
+        axis.ticks = element_line(color = "black", size = 1),
+        axis.ticks.length = unit(0.1, "in"),
         legend.position = "none")
-ggsave(file=paste0("./results/Camacho_Fig3a_",format(Sys.Date(),"%Y-%m-%d"),".pdf"), 
+ggsave(file=paste0("results/", format(Sys.Date(),"%Y-%m-%d"), "_Camacho_Fig3a.", fig_dev),
+       device = fig_dev,
        width = 11, 
        height = 8, 
        units = "in", 
